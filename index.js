eb1e6d4e316a0b7d38db88094f107e6ba332bf93
menachem margolis <margolismendel5728@gmail.com>
2021-02-01 16:04:18 -0500

    commit tree 98d2645a55973abcdd85b49bfdbcce92446416f7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const  gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    let i = 0;
+    while( i < gifts.length){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+        i++
+    }
+   return gifts
+}
+
+
+function writeCards(names,occasion){
+    
+    for(let i = 0; i < names.length; i++ ){
+        console.log(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+        
+    }
+   return names
+}
+
+function counDown(num){
+  for(let i = num; i  > 0 ; i--){
+      console.log(i)
+  }
+}
+counDown(14);
+
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+wrapGifts(gifts);
